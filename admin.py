eefd470b0e81726ad3b5b2528bc73a5afc52adcb
Salman <salman.jsdev@gmail.com>
2022-02-11 21:41:30 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,32 +1,43 @@
 import streamlit as st
 import smtplib, ssl
+import csv
+
+# Getting User Emails
+filename = "members.csv"
+rows = []
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        rows.append(row)
 
 bot_email = "salmandev.bot@gmail.com"
 password = "salmanbotpass"
 port = 587
 smtp_server = "smtp.gmail.com"
 
+st.title("Admin Home")
+st.subheader("Mail Context")
+st.empty()
+mail_subject = st.text_input("Enter Subject : ")
+mail_message  = st.text_area("Enter Message : ")
+main_email_sender = st.button("Send Mail")
+
+mail_content = """\
+Subject: {0}
+{1}
+
+""".format(mail_subject, mail_message)
+
 def sendEmail():
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as email:
         email.starttls(context = context)
         email.login(bot_email, password)
-        email.sendmail(bot_email, reciever_email, main_message)
+        for row in rows:
+            for col in row:
+                reciever_name, reciever_email = col.split()
+                email.sendmail(bot_email, reciever_email, mail_content)
 
-st.title("Admin Home")
-st.subheader("Mail Context")
-st.empty()
-st.write("""
-         
-Subject: PyExpo is about to start!
-
-Hey,
-Thanks for signing up. 
-
-We are happy to inform that PyExpo is going to start in a day. Empower Your Technical Hiring Using Real-Time Coding Interviews.
-Request a Demo! Scale Up Your Technical Hiring Using Our Integrated Coding Assessments and Simulators. 4000+ Clients Globally. 100K+ Questions. 
-24x7 Service Support. Customized Reports. Intuitive User Interface.
-for more quieries: https://salmandevee.web.app/
-
-Happy Coding!""")
-st.button("Send Mail")
+if main_email_sender:
+    sendEmail()
+    st.success("Mail sent successfully")
